d3d327633c4e49341750d064921717032ee278ab
BERTRDOM-SCHOOL <106986652+BERTRDOM-SCHOOL@users.noreply.github.com>
2023-02-09 12:18:01 -0600

    added time, readfile

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+--[[
+luaup
+]]
+
 local file = io.open("main.luaup", "r")
 local m = file:read("*a")
 local opt = {}
@@ -6,7 +10,15 @@ table.insert(opt, [[math.randomseed(os.time())]]) --Gives a random time
 
 table.insert(opt, [[function wait(n)
   os.execute("sleep " .. tonumber(n))
-end]]) --Wait funtion
+end
+]]) --Wait funtion
+
+table.insert(opt, [[function readfile(n)
+  local file = io.open(n, "r")
+  local filereading = file:read("*a")
+  return filereading
+end
+]]) --Readfile
 
 local words = {
     ["set"] = [[local]],
@@ -23,7 +35,10 @@ local words = {
     ["gbl"] = "_G.",
     ["whilelp"] = "while true do",
     ["systime"] = "os.time()",
-    ["clearconsole"] = "os.execute('clear')"
+    ["clearconsole"] = "os.execute('clear')",
+    ["tinsert"]="table.insert",
+    ["tconcat"]="table.concat",
+    ["date"]="os.date()"
 }
 
 local v = m:gsub("%w+", words)
